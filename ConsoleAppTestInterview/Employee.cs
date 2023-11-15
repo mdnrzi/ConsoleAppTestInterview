bb9a311556d870bb565807b13e864baee223687c
mehrdad noruzi <m.norouzi@bahartel.ir>
2023-11-15 14:10:18 -0800

    Add project files.

---
+++ ConsoleAppTestInterview/Employee.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Employee
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+    public int? UserId { get; set; }
+    public int? ManagerId { get; set; }
+    public int Salary { get; set; }
+
+
+
+}
---
+++ ConsoleAppTestInterview/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var stringTest = File.ReadAllText("C:\\Users\\Win10-Norouzi-L1\\Downloads\\sample.txt");
+var employeetModel = JsonConvert.DeserializeObject<List<Employee>>(stringTest);
+
+PrintTopPaid(employeetModel);
+
+
+//Function for showing 2 top salary person
+void PrintTopPaid(List<Employee> employeetModel)
+{
+    var topSalaries = employeetModel.OrderByDescending(e => e.Salary).Take(2).ToList();
+    foreach (Employee employee in topSalaries)
+    {
+        Console.WriteLine($"name:{employee.Name} - lastName: {employee.LastName}");
+    }
+}
